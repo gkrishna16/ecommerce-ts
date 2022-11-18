cb77a1c5f3305d56bb6d8a72e2fc0a8cd4e9bc22
Gopal Krishan <gkrishna160@gmail.com>
2022-11-17 22:56:51 -0500

    added react register and login page

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,8 +1,54 @@
-import React from "react";
-import login from "./login.module.css";
+import React, { useEffect, useState } from "react";
+import lgn from "./login.module.css";
+
+interface loginDetails {
+  userName: string;
+  password: String | any;
+}
 
 const Login = () => {
-  return <div>Login</div>;
+  const [loginInputs, setLoginInputs] = useState<loginDetails>({
+    userName: ``,
+    password: ``,
+  });
+
+  console.log(loginInputs.userName, loginInputs.password);
+
+  function handleSubmit(e: any) {
+    console.log(`submitted`);
+    e.preventDefault();
+
+    fetch(``);
+  }
+  return (
+    <div>
+      <div className={`${lgn.container}`}>
+        <div className="">
+          <label htmlFor="">Username : </label>
+          <input
+            type="text"
+            value={loginInputs.userName}
+            onChange={(e) =>
+              setLoginInputs({ ...loginInputs, userName: e.target.value })
+            }
+          />
+        </div>
+        <div className="">
+          <label htmlFor="">Password : </label>
+          <input
+            type="password"
+            value={loginInputs.password}
+            onChange={(e) =>
+              setLoginInputs({ ...loginInputs, password: e.target.value })
+            }
+          />
+        </div>
+        <div className="">
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -1,8 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import regis from "./register.module.css";
 
+interface registerDetails {
+  userName: string;
+  name: string;
+  password: string;
+  email: string;
+}
+
 const Register = () => {
-  return <div>Register</div>;
+  const [registerInputs, setRegisterInputs] = useState<registerDetails>({
+    userName: ``,
+    name: ``,
+    password: ``,
+    email: ``,
+  });
+
+  console.log(registerInputs);
+
+  function registerSubmit(e: any) {
+    e.preventDefault();
+
+    // fetch();
+  }
+
+  return (
+    <div>
+      <div className={`${regis.regisContainer}`}>
+        <div className={`${regis.formContainer}`}>
+          <div className="">
+            <label htmlFor="">Name : </label>
+            <input
+              type="text"
+              value={registerInputs.name}
+              onChange={(e) =>
+                setRegisterInputs({ ...registerInputs, name: e.target.value })
+              }
+            />
+          </div>
+          <div className="">
+            <label htmlFor="">Email : </label>
+            <input
+              type="text"
+              value={registerInputs.email}
+              onChange={(e) =>
+                setRegisterInputs({ ...registerInputs, email: e.target.value })
+              }
+            />
+          </div>
+          <div className="">
+            <label htmlFor="">Username : </label>
+            <input
+              value={registerInputs.userName}
+              type="text"
+              onChange={(e) =>
+                setRegisterInputs({
+                  ...registerInputs,
+                  userName: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div className="">
+            <label htmlFor="">Password : </label>
+            <input
+              value={registerInputs.password}
+              type="password"
+              onChange={(e) =>
+                setRegisterInputs({
+                  ...registerInputs,
+                  password: e.target.value,
+                })
+              }
+            />
+          </div>
+          <div className="">
+            <button onSubmit={registerSubmit}>Register</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
